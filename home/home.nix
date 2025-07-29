61476cda4d14098674e27954e3994143dcfa6090
Marc Miller <marc.miller@secondfront.com>
2025-07-29 11:41:11 -0400

    add tealdeer

--- home/home.nix
+++ home/home.nix
@@ -80,5 +80,14 @@
         "--disable-features=AutofillSavePaymentMethods"
       ];
     };
+
+    tealdeer = {
+      enable = true;
+      settings = {
+        display.compact = false;
+        display.use_pager = true;
+        updates.auto_update = true;
+      };
+    };
   };
 }
